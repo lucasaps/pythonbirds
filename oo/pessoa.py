a62936859f7f683d74318db1ecfe5dde79eb2406
lucasaps <lucasfla89@hotmail.com>
2022-04-11 19:27:03 -0300

    Criado atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2 #Atributo default ou de classe
     def __init__(self, *filhos, nome=None, idade=100):
         self.idade = idade
         self.nome = nome
@@ -16,6 +17,8 @@ if __name__ == '__main__':
         print(filho.nome)
     ana.sobrenome='Lopes' # atributo criado em tempo de execução
     print(ana.sobrenome)
-    print(ana.__dict__)
+    print(ana.__dict__) # Não exibe atributos de classe
     print(arthur.__dict__)
     del ana.sobrenome
+    print(Pessoa.olhos) # Pode ser acessado pela classe
+    print(ana.olhos)
