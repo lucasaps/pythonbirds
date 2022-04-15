4871f21bcc809ee0d84edbafaea04b5a89bbe11e
lucasaps <lucasfla89@hotmail.com>
2022-04-15 16:49:05 -0300

    Projeto ainda com erros nos testes após todas as aulas

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,11 +6,29 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá, {self.nome}'
+        return f'Olá, meu nome é {self.nome}'
+
+    @classmethod
+    def nome_e_atributos_de_classes(cls):
+        return f'{cls} - olhos: {cls.olhos}'
+
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_classe_pai = super().cumprimentar()
+        return f'{cumprimentar_classe_pai}. Aperte a mão do senhor {self.nome}'
+
+class Mutante(Pessoa):
+    olhos = 4
 
 if __name__ == '__main__':
-    arthur = Pessoa(nome='Arthur')
+    arthur = Homem(nome='Arthur')
     ana = Pessoa(arthur, nome='Ana')
+    mutante = Mutante(nome='Metamorfo')
     print(ana.cumprimentar())
     print(arthur.cumprimentar())
     for filho in ana.filhos:
@@ -22,3 +40,13 @@ if __name__ == '__main__':
     del ana.sobrenome
     print(Pessoa.olhos) # Pode ser acessado pela classe
     print(ana.olhos)
+    print(Pessoa.metodo_estatico())
+    print(arthur.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classes())
+    print(arthur.nome_e_atributos_de_classes())
+    print(isinstance(ana,Pessoa))
+    print(isinstance(ana, Homem))
+    print(isinstance(arthur, Pessoa))
+    print(isinstance(arthur, Homem))
+    print(mutante.olhos)
+    print(mutante.cumprimentar())
